9879edc20aabb65113be918d989dd51f068a52cb
Bernardo Duarte <bernardoduarte.mg@gmail.com>
2024-12-11 15:00:55 -0300

    terminando a 1ª seção do curso

--- src/App.js
+++ src/App.js
@@ -4,7 +4,16 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Hello World React</h1>
+      <h1>Hello World React!</h1>
+      <h2>Emmet funcionou!</h2>
+
+      {/* Fazendo o desafio 1 */}
+      <h3>Lista</h3>
+      <ul>
+        <li>React</li>
+        <li>Next</li>
+        <li>Vue</li>
+      </ul>
     </div>
   );
 }
