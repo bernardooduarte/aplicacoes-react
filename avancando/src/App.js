afb608a28720dde2902356631fe8efe480eb7b33
Bernardo Duarte <bernardoduarte.mg@gmail.com>
2025-01-05 17:40:56 -0300

    aulas 41 a 45

--- avancando/src/App.js
+++ avancando/src/App.js
@@ -1,9 +1,16 @@
 import "./App.css";
 import City from "./assets/city1.jpg";
+import ConditionalRender from "./components/ConditionalRender";
 import ListRender from "./components/ListRender";
 import ManageData from "./components/ManageData";
+import ShowUserName from "./components/ShowUserName";
+import { useState } from "react";
+import CarDetails from "./components/CarDetails";
 
 function App() {
+  const name = "Joaquim";
+  const [userName] = useState("Maria");
+
   return (
     <div className="App">
       <h1>Seção 3</h1>
@@ -16,8 +23,12 @@ function App() {
         <img src={City} alt="Cidade de Nova York" />
       </div>
       <ManageData />
-
       <ListRender />
+      <ConditionalRender />
+      {/* props */}
+      <ShowUserName name={userName} />
+      {/* destructing */}
+      <CarDetails brand="Vw" km={100000} color="Azul" />
     </div>
   );
 }
