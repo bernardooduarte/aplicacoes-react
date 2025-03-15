6de07156e6d05859968122f8ce8c5bbdc5319ca5
Bernardo Duarte <bernardoduarte.mg@gmail.com>
2025-03-15 17:36:56 -0300

    aulas 113 a 115

--- react_router/src/pages/Search.js
+++ react_router/src/pages/Search.js
@@ -1,24 +1,21 @@
-import { Link } from 'react-router-dom'
-import { useFetch } from '../hooks/useFetch'
+import { useSearchParams, Link } from "react-router-dom";
+import { useFetch } from "../hooks/useFetch";
 
-import './Home.css'
+const Search = () => {
+    const [searchParams] = useSearchParams()
 
-const Home = () => {
-    // 3 - carregamento de dados
-    const url = 'http://localhost:3000/products'
+    const url = "http://localhost:3000/products?" + searchParams
 
     const { data: items, loading, error } = useFetch(url);
 
     return (
         <div>
-            <h1>Produtos</h1>
-            {error && <p>{error}</p>}
+            <h1>Resultados disponíveis</h1>
             <ul className="products">
                 {items && items.map((item) => (
                     <li key={item.id}>
                         <h2>{item.name}</h2>
                         <p>R$: {item.price}</p>
-                        {/* 4 - rota dinâmica */}
                         <Link to={`/products/${item.id}`}>Detalhes</Link>
                     </li>
                 ))}
@@ -27,4 +24,4 @@ const Home = () => {
     )
 }
 
-export default Home;
+export default Search
